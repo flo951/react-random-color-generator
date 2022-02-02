7e7c49aafe2cd23779639555668339d383f8c167
Florian Görlich <florian.goerlich95@gmail.com>
2022-02-02 15:55:30 +0100

    Improve code, delete unnecessary components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,50 @@
-import AdvancedButton from './AdvancedButton';
 import './App.css';
+import { useState } from 'react';
+import randomColor from 'randomcolor';
 
-function App() {
+export default function App() {
+  const [hue, setHue] = useState('');
+  const [lum, setLum] = useState('');
+  const [color, setColor] = useState();
+
+  const hueLum = {
+    luminosity: lum,
+    hue: hue,
+  };
+
   return (
-    <div>
-      <AdvancedButton />
+    <div className="body">
+      <div style={{ backgroundColor: color }} className="container">
+        <button
+          style={{ backgroundColor: color }}
+          onClick={() => setColor(randomColor(hueLum))}
+        >
+          Generate
+        </button>
+        <p>Generated Color: {color}</p>
+      </div>
+      <div>
+        {hue} {lum}
+        <div style={{ backgroundColor: color }} />
+      </div>
+      <label>
+        Type in a hue:
+        <input
+          value={hue}
+          onChange={(event) => {
+            setHue(event.currentTarget.value);
+          }}
+        />
+      </label>
+      <label>
+        Type in a lum:
+        <input
+          value={lum}
+          onChange={(event) => {
+            setLum(event.currentTarget.value);
+          }}
+        />
+      </label>
     </div>
   );
 }
-
-export default App;
